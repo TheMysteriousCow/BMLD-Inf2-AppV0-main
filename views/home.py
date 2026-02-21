ef66edc3e865b7d7a394f392a548b14c2657c855
TheMysteriousCow <meissnao@students.zhaw.ch>
2026-02-21 13:47:57 +0100

    first commit

---
+++ views/home.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.title("Meine erste Streamlit App")
+
+# !! WICHTIG: Eure Emails müssen in der App erscheinen!!
+
+"""
+Diese App wurde von folgenden Personen entwickelt:
+- Anna Müller (anmu@zhaw.ch)
+- Peter Meier (peme@zhaw.ch)
+
+Diese App ist das leere Gerüst für die App-Entwicklung im Modul Informatik 2 (BMLD/ZHAW)
+
+Autor: Samuel Wehrli (wehs@zhaw.ch)
+"""
---
+++ views/unterseite_a.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.title("Unterseite A")
+
+st.write("Diese Seite ist eine Unterseite der Startseite.")
